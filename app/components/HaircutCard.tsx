b628c47ee3e4fafb641919fb1157f47546a99d2f
Lichu23 <lisandroxarenax@gmail.com>
2024-10-23 17:25:19 -0300

    fix: fix error build

--- app/components/HaircutCard.tsx
+++ app/components/HaircutCard.tsx
@@ -6,7 +6,6 @@ import { Card } from "@/components/ui/card";
 
 import { buttonVariants } from "@/components/ui/button";
 
-
 const formatPriceArs = new Intl.NumberFormat("es-AR", {
   style: "currency",
   currency: "ARS",
@@ -17,28 +16,24 @@ function HaircutCard() {
   return (
     <div className="flex flex-col p-2 gap-3">
       {haircuts.map((haircut) => (
-
         <Card key={haircut.name}>
-          <Link
-            href={`/haircut/${haircut.name}`}
-            className="flex w-full px-3 py-1.5 flex-col items-start"
-
-        <div key={haircut.name}>
-          <Link
-            
-            href={`/haircuts/${haircut.name.toLowerCase()}`}
-            className={buttonVariants({
-              variant: "outline",
-              className: " py-10 flex flex-col w-full",
-            })}
-
-          >
-            <div className="w-full flex text-2xl max-md:text-xl justify-between font-semibold">
-              <span>{haircut.name}</span>
-              <span>{formatPriceArs.format(haircut.price)}</span>
-            </div>
-            <p className="text-balance text-gray-500">{haircut.description}</p>
-          </Link>
+          <div key={haircut.name}>
+            <Link
+              href={`/haircuts/${haircut.name.toLowerCase()}`}
+              className={buttonVariants({
+                variant: "outline",
+                className: " py-10 flex flex-col w-full",
+              })}
+            >
+              <div className="w-full flex text-2xl max-md:text-xl justify-between font-semibold">
+                <span>{haircut.name}</span>
+                <span>{formatPriceArs.format(haircut.price)}</span>
+              </div>
+              <p className="text-balance text-gray-500">
+                {haircut.description}
+              </p>
+            </Link>
+          </div>
         </Card>
       ))}
     </div>
--- app/views/DashboardView.tsx
+++ app/views/DashboardView.tsx
@@ -7,7 +7,7 @@ function DashboardView() {
   return (
     <main className="h-screen w-full">
       <div className="flex text-white font-semibold flex-col justify-center items-center mt-20">
-        <h1>Don't have reserve?</h1>
+        <h1>Dont have reserve</h1>
         <Link className="bg-black p-2 rounded-xl" href="/haircuts">Reserve here</Link>
       </div>
     </main>
