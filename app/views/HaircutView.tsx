e31ae62cd1f27a241b79839005f8f9b1d8f9ea48
Lichu23 <lisandroarenas@outlook.com>
2024-10-24 16:43:54 -0300

    fix: fix errors

--- app/views/HaircutView.tsx
+++ app/views/HaircutView.tsx
@@ -1,16 +1,14 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
 import React from "react";
 import CardHaircut from "../components/HaircutCard";
 
 async function HaircutView() {
-  const { userId } = auth();
-  const user = await currentUser();
+
 
 
   return (
     <div className="flex flex-col gap-3 justify-center items-center">
       <h1 className="text-3xl mt-5 font-bold text-white">
-        Welcome {user?.firstName}
+        Welcome
       </h1>
       <p className="text-white text-xl">Choose your haircut </p>
       <div className="max-md:w-full w-[600px]">
