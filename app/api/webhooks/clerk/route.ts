4078f287886302df4937e2b961bbc076964c65b5
Lichu23 <lisandroxarenax@gmail.com>
2024-10-24 15:56:35 -0300

    change: Move the lib folder in app folder

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -4,8 +4,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-import { createUser } from "@/lib/actions/user.action";
-import { Z_DATA_ERROR } from "zlib";
+import { createUser } from "@/app/lib/actions/user.action";
 
 export async function POST(req: Request) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
